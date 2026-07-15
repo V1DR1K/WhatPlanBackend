042425525b462aacc21b2c935f66856d511da62a
V1DR1K <V1DR1K@users.noreply.github.com>
2026-07-15 17:43:28 +0200

    Discover nested JPA repositories

--- src/main/java/com/wherefood/WhereFoodApplication.java
+++ src/main/java/com/wherefood/WhereFoodApplication.java
@@ -1,3 +1,11 @@
 package com.wherefood;
-import org.springframework.boot.*; import org.springframework.boot.autoconfigure.*;
-@SpringBootApplication public class WhereFoodApplication { public static void main(String[] args) { SpringApplication.run(WhereFoodApplication.class,args); } }
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(considerNestedRepositories = true)
+public class WhereFoodApplication {
+ public static void main(String[] args) { SpringApplication.run(WhereFoodApplication.class, args); }
+}
